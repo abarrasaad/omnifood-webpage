57f0c33cfd300b8b313c5d3e1345a62ec6049a53
abarrasaad <153920304+abarrasaad@users.noreply.github.com>
2024-09-17 23:37:51 +0100

    Add files via upload
    Implemented sticky navigation, added meta description and favicon and fixed pictures sizes.

--- js/script.js
+++ js/script.js
@@ -45,6 +45,29 @@ allLinks.forEach(function (link) {
 
 ///////////////////////////////////////////////////////////
 
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -62,6 +85,7 @@ function checkFlexGap() {
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
+
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
